28e74443827558814d6819b720c3e11cb4920ad9
Neha Sahu <nehasahu9424@gmail.com>
2021-06-27 18:59:08 +0530

    Menu side bar ,Header ,Footer added for app

--- Ecommerce-Web/src/app/components/product-list/product-list.component.ts
+++ Ecommerce-Web/src/app/components/product-list/product-list.component.ts
@@ -5,7 +5,7 @@ import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.sass'],
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
   products: Product[];
