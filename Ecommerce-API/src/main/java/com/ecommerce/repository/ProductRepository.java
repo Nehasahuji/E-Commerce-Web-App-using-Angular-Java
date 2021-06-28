05c0e38674b613a483e4c1a6931c76fbd60234b0
Neha Sahu <nehasahu9424@gmail.com>
2021-06-28 09:29:40 +0530

    API Added for getting the result by Category

--- Ecommerce-API/src/main/java/com/ecommerce/repository/ProductRepository.java
+++ Ecommerce-API/src/main/java/com/ecommerce/repository/ProductRepository.java
@@ -1,13 +1,18 @@
 package com.ecommerce.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200/")
 @Repository
-public interface ProductRepository  extends JpaRepository<Product, Long>{
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	// added for getting the value by category
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
