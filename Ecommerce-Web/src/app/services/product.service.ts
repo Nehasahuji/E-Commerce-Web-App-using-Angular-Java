a67aa507bd9e01fd60bcc6b860099afa2aed7793
Neha Sahu <nehasahu9424@gmail.com>
2021-06-29 07:40:37 +0530

    New API updated for getting the data product category menu wise

--- Ecommerce-Web/src/app/services/product.service.ts
+++ Ecommerce-Web/src/app/services/product.service.ts
@@ -3,29 +3,45 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product_category';
 
   constructor(private httpClient: HttpClient) {}
 
   getProductList(theCategoryId: number): Observable<Product[]> {
     // need to build URL based on category id
 
-  // const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     return this.httpClient
-      .get<GetResponce>(searchUrl)
+      .get<GetResponceProduct>(searchUrl)
       .pipe(map((response) => response._embedded.products));
+  }
+
+  getProductCategories():Observable<ProductCategory[]>{
+    return this.httpClient
+      .get<GetResponceProductCategory>(this.categoryUrl)
+      .pipe(map((response) => response._embedded.productCategory));
   }
 }
 
-interface GetResponce {
+interface GetResponceProduct {
   _embedded: {
     products: Product[];
+  };
+
+
+}
+
+
+interface GetResponceProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   };
 }
