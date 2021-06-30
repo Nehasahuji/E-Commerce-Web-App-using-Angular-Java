c03a392f0043d593cb63fee2d62058dbcd2b0253
Neha Sahu <nehasahu9424@gmail.com>
2021-06-30 18:47:46 +0530

    Enhance product listComponent to search product

--- Ecommerce-Web/src/app/components/product-list/product-list.component.ts
+++ Ecommerce-Web/src/app/components/product-list/product-list.component.ts
@@ -11,45 +11,65 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
   products: Product[];
   currentCategoryId: number;
-  currentCategoryName :string;
+  currentCategoryName: string;
+  seacrhMode: boolean;
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute
   ) {}
 
-  ngOnInit()
-  {
+  ngOnInit() {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
   }
 
   listProducts() {
+    this.seacrhMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.seacrhMode) {
+      this.handelSearchProducts();
+      console.log('inside search mode');
+    } else {
+      this.handleListProducts();
+    }
+  }
+
+  handelSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    //now search the product using keyword
+
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+      this.products = data;
+      console.log(data);
+    });
+  }
+
+  handleListProducts() {
     //check if  id  parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     console.log(hasCategoryId);
-    
 
-    if(hasCategoryId) {
+    if (hasCategoryId) {
       //get the id param string. convert string to number " using +" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-      console.log( this.currentCategoryId);
+      console.log(this.currentCategoryId);
 
-      //get the name param string. 
+      //get the name param string.
       this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
       console.log(this.currentCategoryName);
-      
-      
     } else {
       //not category id is availbale  ... defaukr to category id 1
       this.currentCategoryId = 1;
-      this.currentCategoryName="Books";
+      this.currentCategoryName = 'Books';
     }
     this.productService
       .getProductList(this.currentCategoryId)
       .subscribe((data) => {
         this.products = data;
+        // console.log(data);
       });
   }
 }
