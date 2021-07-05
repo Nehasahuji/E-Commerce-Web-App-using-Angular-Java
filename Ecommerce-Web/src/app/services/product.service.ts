29e3919b75925fe233e1740a771e1090e741c348
Neha Sahu <nehasahu9424@gmail.com>
2021-07-05 06:57:25 +0530

    Added pagination support to ProductService

--- Ecommerce-Web/src/app/services/product.service.ts
+++ Ecommerce-Web/src/app/services/product.service.ts
@@ -23,7 +23,7 @@ export class ProductService {
   ): Observable<GetResponceProduct> {
     // need to build URL based on category id page and size
     const searchUrl =
-      `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`+
+      `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}` +
       `&page=${thePage}&size=${thePageSize}`;
     return this.httpClient.get<GetResponceProduct>(searchUrl);
   }
@@ -48,6 +48,20 @@ export class ProductService {
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
 
     return this.getProducts(searchUrl);
+  }
+
+  // using pagination for search keyword
+
+  SearchProductPagination(
+    thePage: number,
+    thePageSize: number,
+    theKeyword:string
+  ): Observable<GetResponceProduct> {
+    // need to build URL based on keyword page and size
+    const searchUrl =
+      `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}` +
+      `&page=${thePage}&size=${thePageSize}`;
+    return this.httpClient.get<GetResponceProduct>(searchUrl);
   }
 
   private getProducts(searchUrl: string): Observable<any[]> {
