0cc5c2d2e3ad3359ec8c760223bb1d95b374cdff
Neha Sahu <nehasahu9424@gmail.com>
2021-07-08 09:48:30 +0530

    Added CartService to ProductList

--- Ecommerce-Web/src/app/components/product-list/product-list.component.ts
+++ Ecommerce-Web/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -23,7 +25,8 @@ export class ProductListComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService : CartService
   ) {}
 
   ngOnInit() {
@@ -123,5 +126,9 @@ export class ProductListComponent implements OnInit {
     
 
     // some task to do here
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
+
   }
 }
