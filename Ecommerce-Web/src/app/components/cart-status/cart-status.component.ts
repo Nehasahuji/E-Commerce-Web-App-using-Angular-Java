a54a79847c28ad06a572ea54c46adb9b913d456d
Neha Sahu <nehasahu9424@gmail.com>
2021-07-08 09:49:38 +0530

    Subsrice cartsatuscomponent to cartservice

--- Ecommerce-Web/src/app/components/cart-status/cart-status.component.ts
+++ Ecommerce-Web/src/app/components/cart-status/cart-status.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
   templateUrl: './cart-status.component.html',
-  styleUrls: ['./cart-status.component.css']
+  styleUrls: ['./cart-status.component.css'],
 })
 export class CartStatusComponent implements OnInit {
+  totalPrice: number = 0.0;
+  totalQuantity: number = 0;
 
-  constructor() { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.updateCartStatus();
   }
+  updateCartStatus() {
+    //sunscribe to the cart totalPrice
 
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
+
+    //subscribe to the cart totalquantity
+    this.cartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+  }
 }
