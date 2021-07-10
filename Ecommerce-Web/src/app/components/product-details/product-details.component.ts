308a9393c8183e09ec260ac8023c6f4a04e4be6b
Neha Sahu <nehasahu9424@gmail.com>
2021-07-10 11:09:27 +0530

    Retrive cart Details

--- Ecommerce-Web/src/app/components/product-details/product-details.component.ts
+++ Ecommerce-Web/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,7 +14,9 @@ export class ProductDetailsComponent implements OnInit {
   product: Product = new Product();
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService : CartService
+    
   ) {}
 
   ngOnInit(): void {
@@ -27,5 +31,14 @@ export class ProductDetailsComponent implements OnInit {
     this.productService.getProduct(theProductId).subscribe((data) => {
       this.product = data;
     });
+  }
+
+
+  addToCart(){
+
+    console.log(`Adding to cart : ${this.product.name} unitPrice : ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
+
   }
 }
