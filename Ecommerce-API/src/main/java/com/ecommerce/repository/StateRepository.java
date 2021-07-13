5c38592607daaf7bf83713d4f5c8b7612c95f70d
Neha Sahu <nehasahu9424@gmail.com>
2021-07-13 06:53:44 +0530

    Added Repository for state

---
+++ Ecommerce-API/src/main/java/com/ecommerce/repository/StateRepository.java
@@ -0,0 +1,18 @@
+package com.ecommerce.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.ecommerce.entity.State;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface StateRepository extends JpaRepository<State, Integer> {
+
+	// Retrive states for a given country code
+	List<State> findByCountryCode(@Param("code") String code);
+}
