663f29e7be78a52c6cf5388a9c24503b3b40ad48
Neha Sahu <nehasahu9424@gmail.com>
2021-07-17 11:55:02 +0530

    Added CustomerRepository

---
+++ Ecommerce-API/src/main/java/com/ecommerce/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.entity.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
