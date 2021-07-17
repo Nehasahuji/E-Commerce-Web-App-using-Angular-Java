e5a9817461739a193c5b84354f8dd79841756f14
Neha Sahu <nehasahu9424@gmail.com>
2021-07-17 11:51:15 +0530

    Added Customer entity

---
+++ Ecommerce-API/src/main/java/com/ecommerce/entity/Customer.java
@@ -0,0 +1,51 @@
+package com.ecommerce.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+	private Set<Order> orders = new HashSet<>();
+
+	public void add(Order order) {
+		if (order != null) {
+			if (orders == null) {
+				orders = new HashSet<Order>();
+			}
+
+			orders.add(order);
+			order.setCustomer(this);
+		}
+	}
+}
