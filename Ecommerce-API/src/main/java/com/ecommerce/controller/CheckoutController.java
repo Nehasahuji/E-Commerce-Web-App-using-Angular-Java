6c533dd8cc2d6bc9d5075d56a493a9ddfa218f45
Neha Sahu <nehasahu9424@gmail.com>
2021-07-17 11:55:36 +0530

    controller

---
+++ Ecommerce-API/src/main/java/com/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,32 @@
+package com.ecommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.dto.Purchase;
+import com.ecommerce.dto.PurchaseResponce;
+import com.ecommerce.service.CheckoutService;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout/")
+public class CheckoutController {
+
+	private CheckoutService checkoutService;
+
+	@Autowired
+	public CheckoutController(CheckoutService checkoutService) {
+
+		this.checkoutService = checkoutService;
+	}
+
+	@PostMapping("/purchase")
+	public PurchaseResponce placeorder(@RequestBody Purchase purchase) {
+		PurchaseResponce purchaseResponce = checkoutService.placeOrder(purchase);
+		return purchaseResponce;
+	}
+}
