bd9fa7327f4b2ea90d0f7e9e2522c39ff34035b6
Neha Sahu <nehasahu9424@gmail.com>
2021-07-20 18:50:36 +0530

    added login/logout component for login login uptton

---
+++ Ecommerce-Web/src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css']
+})
+export class LoginStatusComponent implements OnInit {
+
+
+  isAuthenticated : boolean=false;
+  userFullName: string;
+  constructor(private oktaAuthService : OktaAuthService) { }
+
+  ngOnInit(): void {
+
+    // subscibe to the authenticateion state
+      this.oktaAuthService.$authenticationState.subscribe(
+        (result) =>{
+          this.isAuthenticated = result;
+          this.getUserDetails();
+        }
+      )
+
+  }
+  getUserDetails() {
+    
+    if(this.isAuthenticated){
+      // fetch the logged in user details ( user's claims)
+
+
+
+
+      // user full name is exposed as a property
+
+      this.oktaAuthService.getUser().then(
+        (res) =>{
+          this.userFullName = res.name!;
+        }
+      );
+    }
+    
+  }
+
+
+
+  logout(){
+  //  terminates the session with okta and removes current tokens
+           this.oktaAuthService.signOut();
+  }
+
+}
+
+
