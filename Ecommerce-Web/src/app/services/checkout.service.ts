f4197947b2c394060234f012ea84e076d75e25d5
Neha Sahu <nehasahu9424@gmail.com>
2021-08-10 19:59:21 +0530

    Updated for tracking using email id

--- Ecommerce-Web/src/app/services/checkout.service.ts
+++ Ecommerce-Web/src/app/services/checkout.service.ts
@@ -8,7 +8,7 @@ import { Purchase } from '../common/purchase';
 })
 export class CheckoutService {
   
-  private purchaseUrl = 'http://localhost:8080/api/checkout/purchase'
+  private purchaseUrl = 'http://localhost:8080/api/checkout/purchase';
   
   constructor(private httpClient : HttpClient) { }
 
