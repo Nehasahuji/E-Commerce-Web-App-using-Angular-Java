c12c148a7ec4b5f88251d08a449292af3712cdcf
Neha Sahu <nehasahu9424@gmail.com>
2021-08-11 09:06:31 +0530

    Added Security Configuration for Order-History Url

---
+++ Ecommerce-API/src/main/java/com/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.okta.spring.boot.oauth.Okta;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+//		protect endpoind /api/orders
+
+		http.authorizeRequests().antMatchers("/api/orders/**").authenticated().and().oauth2ResourceServer().jwt();
+
+		http.cors();
+
+//		force a non empty response body for 401's to make the response more friendly
+		Okta.configureResourceServer401ResponseBody(http);
+
+	}
+
+}
