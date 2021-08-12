8ad532b1235f9926cf8814b380a7a676219a9b73
Neha Sahu <nehasahu9424@gmail.com>
2021-08-12 07:32:36 +0530

    Updated app.module.ts to reference interceptor

--- Ecommerce-Web/src/app/app.module.ts
+++ Ecommerce-Web/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
@@ -27,6 +27,7 @@ import myAppConfig from './config/my-app-config';
 import { Router } from '@angular/router';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { AuthInterceptorService } from './services/interceptor/auth-interceptor.service';
 
 const oktaConfig = Object.assign(
   {
@@ -64,7 +65,8 @@ const oktaConfig = Object.assign(
     OktaAuthModule,
   ],
 
-  providers: [ProductService, { provide: OKTA_CONFIG, useValue: oktaConfig }],
+  providers: [ProductService, { provide: OKTA_CONFIG, useValue: oktaConfig },
+             {provide :HTTP_INTERCEPTORS,useClass :AuthInterceptorService ,multi :true}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
