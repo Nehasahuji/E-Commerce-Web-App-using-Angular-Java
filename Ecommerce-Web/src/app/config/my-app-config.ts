66d613834cdf43f9889d6263c3092ffb1394bca9
Neha Sahu <nehasahu9424@gmail.com>
2021-08-13 11:31:19 +0530

    Updated app config for https secure-connection

--- Ecommerce-Web/src/app/config/my-app-config.ts
+++ Ecommerce-Web/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa1a5l1ulJRl39P55d7',
     issuer: 'https://dev-76640188.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 };
